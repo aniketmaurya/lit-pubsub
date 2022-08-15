108abd8c11b88967a897ff7a65dcbab99ca4111e
Aniket Maurya <theaniketmaurya@gmail.com>
2022-08-15 11:15:27 +0530

    setup

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="lit_pubsub",
-    version="0.0.0",
-    description="Describe Your Cool Component",
-    author="",
-    author_email="",
+    version="0.0.1",
+    description="Lightning AI integration for publisher subscriber messaging queues like Kafka and AWS SQS",
+    author="Aniket Maurya",
+    author_email="theaniketmaurya@gmail.com",
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     url="https://github.com/aniketmaurya/lit-pubsub",
     install_requires=[],
